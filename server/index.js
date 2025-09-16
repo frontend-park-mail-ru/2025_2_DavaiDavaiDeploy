86a72fdb0fe6f4bac5fe0501c114c9826170f9f3
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-09-16 11:43:28 +0300

    chore(server): enhance Express server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import reload from "reload";
+import {execSync} from "child_process";
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('X-Powered-By', 'Express');
+  res.statusCode = 200;
+  console.log(`[${req.method}] ${req.url} ${res.statusCode}`);
+  next();
+});
 
 app.use('/public', express.static('public'));
 
@@ -10,4 +19,8 @@ app.get('/', (_, response) => {
 
 app.listen(3000, () => {
   console.log('Server running on http://127.0.0.1:3000');
+});
+
+reload(app).then(function () {
+  execSync("npm run build", { stdio: 'inherit', shell: true });
 });
