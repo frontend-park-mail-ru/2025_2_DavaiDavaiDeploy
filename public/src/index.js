5b8c4831c2abd38c76fc2fb8b064b5b20c0fb1bb
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-09-16 11:31:27 +0300

    feat(templates): integrate Handlebars with test template

---
+++ public/src/index.js
@@ -0,0 +1,9 @@
+import '../build/precompiled.js';
+import Test from './test/test.js';
+
+const root = document.createElement('div');
+root.id = 'root';
+document.body.appendChild(root);
+
+const test = new Test(root, {text: 'Working'});
+test.render();
--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.use('/src', express.static('src'));
 app.use('/public', express.static('public'));
 
 app.get('/', (_, response) => {
