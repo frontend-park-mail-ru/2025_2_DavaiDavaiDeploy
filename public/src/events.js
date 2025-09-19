a3c778e9e1e4f37762e09eb426ab5b83d9bf516f
Robocotik <alexander.starkin@gmail.com>
2025-09-19 20:20:10 +0300

    [choire] make few code inline style

--- public/src/events.js
+++ public/src/events.js
@@ -6,8 +6,7 @@ export function addEventListener(eventName, handler, el) {
 export function addEventListeners(listeners = {}, el) {
   const addedListeners = {};
   Object.entries(listeners).forEach(([eventName, handler]) => {
-    const listener = addEventListener(eventName, handler, el);
-    addedListeners[eventName] = listener;
+    addedListeners[eventName] = addEventListener(eventName, handler, el);
   });
   return addedListeners;
 }
