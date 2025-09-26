187541c82a3f68b6f5032e6654c9264e06035000
Robocotik <alexander.starkin@gmail.com>
2025-09-26 17:08:31 +0300

    [feat] page: do new registrate page

--- src/pages/registrationPage/registrationPage.js
+++ src/pages/registrationPage/registrationPage.js
@@ -16,7 +16,34 @@ export default class RegistrationPage {
 		this.#parent.innerHTML = ''
 		this.#self = document.createElement('div')
 		this.#self.id = 'registration-page'
+		this.#self.classList.add('registration-page')
 		this.#parent.appendChild(this.#self)
 		this.#self.insertAdjacentHTML('afterbegin', this.template)
+
+		document.querySelector('#password-icon').addEventListener('click', () => {
+			const passwordInput = document.querySelector('#password')
+			const passwordType =
+				passwordInput.getAttribute('type') === 'password' ? 'text' : 'password'
+			passwordInput.setAttribute('type', passwordType)
+			document.querySelector('#password-icon').src =
+				passwordType === 'password'
+					? '/src/assets/img/eye_close.svg'
+					: '/src/assets/img/eye_open.svg'
+		})
+
+		document
+			.querySelector('#confirm-password-icon')
+			.addEventListener('click', () => {
+				const passwordInput = document.querySelector('#confirm-password')
+				const passwordType =
+					passwordInput.getAttribute('type') === 'password'
+						? 'text'
+						: 'password'
+				passwordInput.setAttribute('type', passwordType)
+				document.querySelector('#confirm-password-icon').src =
+					passwordType === 'password'
+						? '/src/assets/img/eye_close.svg'
+						: '/src/assets/img/eye_open.svg'
+			})
 	}
 }
