297ced107e9808cd60c39d56369d540af6adf698
Robocotik <alexander.starkin@gmail.com>
2025-09-28 22:45:15 +0300

    [feat] add env.js

--- src/consts/serverAddr.js
+++ src/consts/serverAddr.js
@@ -1,1 +1,5 @@
-export const serverAddr = 'http://localhost:5458'
+import { isProduction } from './env.js'
+
+export const serverAddr = isProduction
+	? 'http://ddfilms.online:8080/api'
+	: 'http://localhost:5458/api'
