5b2277700e9628a944190be506eb7be2c664fa1b
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-09-29 13:01:42 +0300

    fix: fix server adress

--- src/consts/serverAddr.js
+++ src/consts/serverAddr.js
@@ -2,8 +2,8 @@ import { isProduction } from './../env.js'
 
 export const serverAddr = isProduction
 	? 'https://ddfilms.online/api'
-	: 'https://localhost:5458/api'
+	: 'http://localhost:5458/api'
 
 export const serverAddrForStatic = isProduction
 	? 'https://ddfilms.online'
-	: 'https://localhost:5458'
+	: 'http://localhost:5458'
