a82105d5c69e211c162e7a1b1dd2d42ffc0fb46a
Robocotik <alexander.starkin@gmail.com>
2025-09-29 03:57:27 +0300

    [feat] jsdoc: all pages

--- src/pages/loginPage/loginPage.js
+++ src/pages/loginPage/loginPage.js
@@ -1,19 +1,31 @@
 import LoginForm from '../../components/loginForm/loginForm.js'
-
+/**
+ * Класс для отображения страницы входа.
+ */
 export default class LoginPage {
 	#parent
 	#unsubscribe
 
+	/**
+	 * @param {HTMLElement} rootElement - Родительский DOM-элемент.
+	 */
 	constructor(rootElement) {
 		this.#parent = rootElement
 	}
 
+	/**
+	 * Шаблон страницы входа.
+	 * @returns {string}
+	 */
 	get template() {
 		return Handlebars.templates[`loginPage.hbs`]({
 			text: 'Login',
 		})
 	}
 
+	/**
+	 * Рендерит страницу входа и форму.
+	 */
 	render() {
 		this.#parent.innerHTML = ''
 		this.#parent.insertAdjacentHTML('afterbegin', this.template)
@@ -22,6 +34,9 @@ export default class LoginPage {
 		form.render()
 	}
 
+	/**
+	 * Очистка/отписка от событий (если реализовано).
+	 */
 	destroy() {
 		this.#unsubscribe?.()
 	}
