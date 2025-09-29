5fe6ae009d196f7a0f19098e7995cf560e6ba21c
Robocotik <alexander.starkin@gmail.com>
2025-09-29 04:04:41 +0300

    [feat] jsdoc: all redux features + shared

--- src/redux/features/genre/reducers.js
+++ src/redux/features/genre/reducers.js
@@ -1,11 +1,22 @@
 import types from './types.js'
 
+/**
+ * Начальное состояние редьюсера жанров.
+ * @type {{ loading: boolean, genres: Array<Object>, error: string | null }}
+ */
 const initialState = {
 	loading: false,
 	genres: [],
 	error: null,
 }
 
+/**
+ * Редьюсер для управления состоянием жанров.
+ *
+ * @param {typeof initialState} state - Текущее состояние.
+ * @param {{ type: string, payload?: any }} action - Action.
+ * @returns {typeof initialState} Новое состояние.
+ */
 const genreReducer = (state = initialState, action) => {
 	const { type, payload } = action
 
