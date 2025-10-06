b3863acd6b9584eb632dcc21b5357ad5cff7100c
Robocotik <alexander.starkin@gmail.com>
2025-10-06 16:04:15 +0300

    [feat] project: add statefull components

--- public/src/attributes.js
+++ public/src/attributes.js
@@ -1,3 +1,10 @@
+export function setStyle(el, name, value) {
+  el.style[name] = value;
+}
+export function removeStyle(el, name) {
+  el.style[name] = null;
+}
+
 export function setAttributes(el, attrs) {
   const {class: className, style, ...otherAttrs} = attrs;
   if (className) {
