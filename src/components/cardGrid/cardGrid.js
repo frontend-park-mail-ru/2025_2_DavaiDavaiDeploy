542009533b00276d783c041c2ea099495d00f8e1
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-10-12 02:02:42 +0300

    refactor(card grid): remove virtualization, add duplicate avoidance

--- src/components/cardGrid/cardGrid.js
+++ src/components/cardGrid/cardGrid.js
@@ -9,20 +9,17 @@ import {
 } from '../../redux/features/film/selectors.js'
 import { store } from '../../redux/store.js'
 import Component from '../core/baseComponent.js'
-import FilmCardPlaceholder from '../filmCardPlaceholder/filmCardPlaceholder.js'
 
 const UPLOADING_ROWS_COUNT = 3
-const ROWS_IN_BUFFER = 1
 const THROTTLE_DELAY = 100
+const ROOTMARGIN = '500px'
 
 export default class CardGrid extends Component {
 	#unsubscribe
 	#offset = 0
 	#uploadAllFilms = false
-	#cards = {}
-	#windowHeight
-	#throttledResizeHandler
-	#animationFrameId = null
+	#throttledIntersectHandler
+	#receivedFilms = new Set()
 
 	constructor(parent, props = {}) {
 		super(parent, props, 'cardGrid')
@@ -36,39 +33,42 @@ export default class CardGrid extends Component {
 		return this.self?.querySelector('.grid')
 	}
 
+	get trigger() {
+		return this.self?.querySelector('.load-more-trigger')
+	}
+
 	render() {
 		this.parent.insertAdjacentHTML('beforeend', this.html())
 
 		this.#unsubscribe = store.subscribe(this.handleStoreUpdate)
 
-		this.handleResize()
+		this.#throttledIntersectHandler = throttle(
+			this.handleIntersect,
+			THROTTLE_DELAY,
+		)
 
-		const cardsPerRow = getGridColumnCount(this.grid)
-		this.loadMoreFilms(cardsPerRow * UPLOADING_ROWS_COUNT)
-
-		this.#throttledResizeHandler = throttle(() => {
-			requestAnimationFrame(() => {
-				this.handleResize()
-			})
-		}, THROTTLE_DELAY)
-
-		window.addEventListener('resize', this.#throttledResizeHandler)
-
-		this.update()
+		const observer = new IntersectionObserver(this.#throttledIntersectHandler, {
+			rootMargin: ROOTMARGIN,
+		})
+		observer.observe(this.trigger)
 	}
 
-	handleStoreUpdate = () => {
-		const films = selectFilmSection(store.getState())
-		this.addNewCards(films)
+	handleIntersect = () => {
+		const cardsPerRow = getGridColumnCount(this.grid)
+		this.loadMoreFilms(cardsPerRow * UPLOADING_ROWS_COUNT)
 	}
 
 	loadMoreFilms = count => {
 		if (this.#uploadAllFilms) {
 			return
 		}
-
 		store.dispatch(filmActions.getFilmsAction(count, this.#offset))
 		this.#offset += count
+	}
+
+	handleStoreUpdate = () => {
+		const films = selectFilmSection(store.getState())
+		this.addNewCards(films)
 	}
 
 	addNewCards = state => {
@@ -78,133 +78,30 @@ export default class CardGrid extends Component {
 		if (state.error) {
 			this.#uploadAllFilms = true
 		}
-	}
-
-	update = () => {
-		this.#animationFrameId = requestAnimationFrame(this.update)
-		this.updateViewport()
-	}
-
-	handleResize = () => {
-		this.#windowHeight = window.innerHeight
-
-		const card = Array.from(document.querySelectorAll('.film-card')).find(
-			el => !el.querySelector('.placeholder'),
-		)
-
-		if (!card) {
-			return
-		}
-
-		const cardWidth = card.offsetWidth
-		const cardHeight = card.offsetHeight
-
-		for (const card of Object.values(this.#cards)) {
-			card.resizePlaceholder(cardWidth, cardHeight)
-		}
-
-		this.updateViewport()
-	}
-
-	updateViewport = () => {
-		const { startIndex, endIndex } = this.getVisibleCards()
 
 		const films = selectFilms(store.getState())
-
 		for (let i = 0; i < films.length; i++) {
-			if (startIndex <= i && i < endIndex) {
-				this.renderFilm(films[i])
-			} else {
-				this.replaceFilm(films[i])
-			}
+			this.renderFilm(films[i])
 		}
-	}
-
-	getVisibleCards = () => {
-		const cardsPerRow = getGridColumnCount(this.grid)
-		const card = this.grid.querySelector('.film-card')
-
-		if (!card) {
-			return { startIndex: 0, endIndex: cardsPerRow }
-		}
-
-		const cardHeight = this.grid.querySelector('.film-card').offsetHeight
-
-		const gridRect = this.grid.getBoundingClientRect()
-		const gridRectTop = gridRect.top
-
-		if (this.#windowHeight < gridRectTop) {
-			return { startIndex: 0, endIndex: cardsPerRow }
-		}
-
-		let invisibleTopHeight = Math.abs(gridRectTop)
-		let visibleRows = Math.ceil(window.innerHeight / cardHeight)
-
-		if (gridRectTop >= 0) {
-			invisibleTopHeight = 0
-			visibleRows = Math.ceil((this.#windowHeight - gridRectTop) / cardHeight)
-		}
-
-		const rowsBeforeStart = Math.max(
-			Math.trunc(invisibleTopHeight / cardHeight) - ROWS_IN_BUFFER,
-			0,
-		)
-
-		const rowsInViewPort = visibleRows + 2 * ROWS_IN_BUFFER
-
-		let startIndex = rowsBeforeStart * cardsPerRow
-		let endIndex = startIndex + rowsInViewPort * cardsPerRow
-
-		const films = selectFilms(store.getState())
-		const length = films.length
-
-		if (endIndex > length) {
-			endIndex = length
-			startIndex = Math.max(0, endIndex - rowsInViewPort * cardsPerRow)
-
-			this.loadMoreFilms(cardsPerRow * UPLOADING_ROWS_COUNT)
-		}
-
-		return { startIndex, endIndex }
 	}
 
 	renderFilm = film => {
-		const card = this.#cards[film.id]
-		if (!card) {
-			const filmCard = new FilmCard(this.grid, {
-				id: film.id,
-				image: `${serverAddrForStatic}${film.icon}`,
-				title: film.title,
-				info: `${film.genres[0].title}, ${film.year}`,
-				rating: film.rating,
-			})
-			this.#cards[film.id] = filmCard
-			filmCard.render()
+		if (this.#receivedFilms.has(film.id)) {
 			return
 		}
-		const placeholder = card.self.querySelector('.placeholder')
-		if (placeholder) {
-			card.rerender()
-		}
-	}
-
-	replaceFilm = film => {
-		const filmCard = document.querySelector(`#film-${film.id}`)
-		if (!filmCard) {
-			return
-		}
-		const child = filmCard.querySelector('.placeholder')
-		if (child) {
-			return
-		}
-		const placeholder = new FilmCardPlaceholder(filmCard)
-		placeholder.render()
+		const filmCard = new FilmCard(this.grid, {
+			id: film.id,
+			image: `${serverAddrForStatic}${film.icon}`,
+			title: film.title,
+			info: `${film.genres[0].title}, ${film.year}`,
+			rating: film.rating,
+		})
+		this.#receivedFilms.add(film.id)
+		filmCard.render()
 	}
 
 	destroy() {
 		this.#unsubscribe?.()
-
-		window.removeEventListener('resize', this.#throttledResizeHandler)
 
 		store.dispatch(filmActions.clearFilmsAction())
 		this.#uploadAllFilms = false
@@ -212,8 +109,5 @@ export default class CardGrid extends Component {
 		if (this.self) {
 			this.self.innerHTML = ''
 		}
-
-		cancelAnimationFrame(this.#animationFrameId)
-		this.#animationFrameId = null
 	}
 }
