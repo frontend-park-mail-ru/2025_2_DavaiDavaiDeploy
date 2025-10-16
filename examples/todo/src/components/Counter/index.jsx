1231f9a758453df58044a31362d5aa879b1acef6
Robocotik <alexander.starkin@gmail.com>
2025-10-16 21:29:55 +0300

    [feat]  lib: add children normalization

--- examples/todo/src/components/Counter/index.jsx
+++ examples/todo/src/components/Counter/index.jsx
@@ -18,7 +18,7 @@ export class Counter extends Component {
 
     return (
       <div>
-        <p>Count: {count.toString()}</p>
+        <p>Count: {count}</p>
         <button onClick={onIncrement}>Increment</button>
       </div>
     );
