2645e14d8e6e196ff31fd85117d37a0370d4683d
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-10-19 00:18:24 +0300

    refactor(typescript): improve type organization

--- src/helpers/throttleHelper/throttleHelper.ts
+++ src/helpers/throttleHelper/throttleHelper.ts
@@ -1,12 +1,11 @@
+interface ThrottledFunction {
+	(...args: any[]): void
+}
 /**
  * Создаёт "троттлированную" версию функции, которая вызывается не чаще одного раза за указанный интервал.
  */
-export const throttle = (
-	func: Function,
-	wait: number,
-): ((...args: any[]) => void) => {
+export const throttle = (func: Function, wait: number): ThrottledFunction => {
 	let isCalled = false
-
 	return function (this: any, ...args: any[]) {
 		if (!isCalled) {
 			func.apply(this, args)
