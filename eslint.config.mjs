ad78ff19b2bee3621ea46f8e789cd913e12df986
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-10-19 23:28:58 +0300

    chore(linters): add TypeScript support and SonarJS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,21 @@
 import js from '@eslint/js'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import compatPlugin from 'eslint-plugin-compat'
+import sonarjs from 'eslint-plugin-sonarjs'
 import { defineConfig } from 'eslint/config'
 import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
 	{
 		files: ['**/*.{js,mjs,cjs}'],
 		plugins: { js, compat: compatPlugin },
-		extends: ['js/recommended', eslintConfigPrettier],
-		languageOptions: {
-			globals: {
-				...globals.browser,
-			},
-		},
+		extends: [
+			'js/recommended',
+			eslintConfigPrettier,
+			sonarjs.configs.recommended,
+		],
+		languageOptions: { globals: { ...globals.browser } },
 		rules: {
 			'no-unused-vars': 'error',
 			'no-undef': 'error',
@@ -22,13 +24,60 @@ export default defineConfig([
 			'no-console': 'error',
 			'no-multiple-empty-lines': 'error',
 			'no-useless-return': 'error',
+			'sonarjs/cognitive-complexity': ['error', 15],
+			'sonarjs/no-identical-functions': 'error',
+			'sonarjs/no-all-duplicated-branches': 'error',
+			'sonarjs/no-duplicate-string': 'error',
+			'sonarjs/prefer-immediate-return': 'error',
 		},
 		ignores: [
 			'node_modules/',
 			'**/build/**',
 			'.template-lintrc.js',
 			'.lintstagedrc.js',
-			'dist/',
+			'dist/**',
 		],
+	},
+
+	{
+		files: ['**/*.{ts}'],
+		plugins: {
+			'@typescript-eslint': tseslint.plugin,
+			compat: compatPlugin,
+		},
+		extends: [
+			...tseslint.configs.recommended,
+			eslintConfigPrettier,
+			sonarjs.configs.recommended,
+		],
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {
+				project: ['./tsconfig.json'],
+				sourceType: 'module',
+				ecmaVersion: 'latest',
+			},
+			globals: {
+				...globals.browser,
+			},
+		},
+		rules: {
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{ argsIgnorePattern: '^_' },
+			],
+			'@typescript-eslint/no-explicit-any': 'warn',
+			curly: 'error',
+			'compat/compat': 'error',
+			'no-console': 'error',
+			'no-multiple-empty-lines': 'error',
+			'no-useless-return': 'error',
+			'sonarjs/cognitive-complexity': ['error', 15],
+			'sonarjs/no-identical-functions': 'error',
+			'sonarjs/no-all-duplicated-branches': 'error',
+			'sonarjs/no-duplicate-string': 'error',
+			'sonarjs/prefer-immediate-return': 'error',
+		},
+		ignores: ['node_modules/', '**/build/**', 'dist/**'],
 	},
 ])
