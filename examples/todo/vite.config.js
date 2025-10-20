97fb12104f3310080167153d3aa29d92b2372069
Robocotik <alexander.starkin@gmail.com>
2025-10-21 00:24:37 +0300

    [choire] make code prettier

--- examples/todo/vite.config.js
+++ examples/todo/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     jsx: 'transform',
     jsxFactory: 'jsx',
     jsxFragment: 'Fragment',
-    jsxInject: "import {jsx, Fragment} from '@lib/index.d.ts'",
+    jsxInject: "import {jsx, Fragment} from '@lib/react.d.ts'",
   },
   server: {
     port: 3000,
@@ -21,8 +21,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@lib': resolve(__dirname, '../../lib/dist'),
-      '@types': resolve(__dirname, '../../lib/src/types'),
-      '@utils': resolve(__dirname, '../../lib/src/utils'),
       '@src': resolve(__dirname, 'src'),
     },
   },
