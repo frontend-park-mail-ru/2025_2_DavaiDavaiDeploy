4a67fd85f8963219778e30d38364eb5d86f345e3
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-10-21 20:24:33 +0300

    chore: add example

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
 import '@/styles/globals.scss';
 import 'reset-css/reset.css';
+
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+	dsn: import.meta.env.VITE_SENTRY_DSN,
+	enabled: import.meta.env.PROD,
+});
+
+let btn = document.createElement('button');
+btn.addEventListener('click', () => {
+	Sentry.captureMessage('Hello, world!');
+	Sentry.captureException(new Error('Good bye'));
+});
+
+document.body.append(btn);
