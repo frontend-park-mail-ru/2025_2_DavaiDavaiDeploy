d790a14f68f28f36287fdcacf7a6b8d4aaee7b1c
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-10-21 21:32:13 +0300

    refactor: change configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,34 @@
-import { defineConfig } from 'vite';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
+import type { ConfigEnv } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-	plugins: [tsconfigPaths()],
-	server: {
-		host: 'localhost',
-		port: 3000,
-	},
-	build: {
-		outDir: 'dist',
-		emptyOutDir: true,
-		assetsDir: 'assets',
-		sourcemap: true,
-	},
-});
+export default ({ mode }: ConfigEnv) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+	// import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+	// import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+	return defineConfig({
+		plugins: [
+			tsconfigPaths(),
+			sentryVitePlugin({
+				org: process.env.SENTRY_ORG,
+				project: process.env.SENTRY_PROJECT,
+
+				// Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
+				authToken: process.env.SENTRY_AUTH_TOKEN,
+			}),
+		],
+		server: {
+			host: 'localhost',
+			port: 3000,
+		},
+		build: {
+			outDir: 'dist',
+			emptyOutDir: true,
+			assetsDir: 'assets',
+			sourcemap: true,
+		},
+	});
+};
