8eaedaea191e2bdcf7bae11e9bd27b5fe0a41c4b
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-10-21 20:38:55 +0300

    chore: fix example

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ import * as Sentry from '@sentry/browser';
 Sentry.init({
 	dsn: import.meta.env.VITE_SENTRY_DSN,
 	enabled: import.meta.env.PROD,
+	integrations: [Sentry.browserTracingIntegration()],
+	tracePropagationTargets: [
+		'localhost', // For local development
+		'https://ddfilms.online/', // For your backend domain
+	],
 });
 
 let btn = document.createElement('button');
