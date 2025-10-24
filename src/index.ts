ea1d99441524e114f5b47b461347c03f84082009
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-10-24 16:39:16 +0300

    feat: add Sentry check

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import 'reset-css/reset.css';
 
 import * as Sentry from '@sentry/browser';
 
-Sentry.init({
-	dsn: import.meta.env.VITE_SENTRY_DSN,
-	enabled: import.meta.env.PROD,
-	integrations: [Sentry.browserTracingIntegration()],
-	tracePropagationTargets: ['https://ddfilms.online/'],
-});
+if (import.meta.env.VITE_SENTRY_ENABLED) {
+	Sentry.init({
+		dsn: import.meta.env.VITE_SENTRY_DSN,
+		enabled: import.meta.env.PROD,
+		integrations: [Sentry.browserTracingIntegration()],
+		tracePropagationTargets: ['https://ddfilms.online/'],
+	});
+}
 
 let btn = document.createElement('button');
 btn.addEventListener('click', () => {
