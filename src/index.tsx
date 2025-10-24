039399e9dbfdbc4c5869fa4af4e2878c1b1bfa23
Robocotik <alexander.starkin@gmail.com>
2025-10-24 14:07:31 +0300

    [feat]examples: add multipge + redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import { Component, render } from '@react';
 import { connect, Provider } from './modules/redux';
 import type { Dispatch } from './modules/redux/types/actions.ts';
 import type { State } from './modules/redux/types/store.ts';
+import { Route } from './modules/router/route.tsx';
+import { RouterProvider } from './modules/router/RouterProvider.tsx';
+import { Routes } from './modules/router/routes.tsx';
+
+import { Link } from './modules/router/link.tsx';
 import { store } from './redux/store.ts';
 
 interface MyComponentProps {
@@ -35,9 +40,65 @@ const ConnectedMyComponent = connect(
 	mapDispatchToProps,
 )(MyComponent);
 
+class MainApp extends Component {
+	render() {
+		return (
+			<div>
+				<p>Главная</p>
+				<Link href="/about">Back to About</Link>
+				<br></br>
+				<Link href="/contact/4?sosal=52">Back to contact</Link>
+				<ConnectedMyComponent />
+			</div>
+		);
+	}
+}
+
+class AboutApp extends Component {
+	render() {
+		return (
+			<div>
+				<p>about</p>
+				<Link href="/">Back to Home</Link>
+				<br></br>
+				<Link href="/contact/4?sosal=52">Back to contact</Link>
+				<ConnectedMyComponent />
+			</div>
+		);
+	}
+}
+
+class ContactIDApp extends Component {
+	render() {
+		return (
+			<div>
+				<p>Contact ID</p>
+				<Link href="/">Back to Home</Link>
+				<br></br>
+				<Link href="/about">Back to About</Link>
+				<ConnectedMyComponent />
+			</div>
+		);
+	}
+}
+
+class App extends Component {
+	render() {
+		return (
+			<RouterProvider>
+				<Routes>
+					<Route href="/" component={<MainApp />} />
+					<Route href="/about" component={<AboutApp />} />
+					<Route href="/contact/:id" component={<ContactIDApp />} />
+				</Routes>
+			</RouterProvider>
+		);
+	}
+}
+
 render(
 	<Provider store={store}>
-		<ConnectedMyComponent />
+		<App />
 	</Provider>,
 	document.body,
 );
