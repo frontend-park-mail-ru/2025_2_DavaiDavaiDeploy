d47068865e3aa83773504612b85961a63a004358
Robocotik <alexander.starkin@gmail.com>
2025-10-29 01:40:16 +0300

    [feat] it's alive

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default ({ mode }: ConfigEnv) => {
 			jsx: 'transform',
 			jsxFactory: 'jsx',
 			jsxFragment: 'Fragment',
-			jsxInject: "import {jsx, Fragment} from '@robocotik/react'",
+			jsxInject: "import {jsx, Fragment} from '@robocotik/react/jsx-runtime'",
 			jsxDev: false,
 		},
 		server: {
