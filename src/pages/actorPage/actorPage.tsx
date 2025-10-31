76890346121d4d848f09fe1b220f3f0683c28dde
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-11-01 00:00:17 +0300

    refactor: add map delete

--- src/pages/actorPage/actorPage.tsx
+++ src/pages/actorPage/actorPage.tsx
@@ -3,11 +3,8 @@ import { FilmSlider } from '@/components/filmSlider/filmSlider';
 import { Component } from '@robocotik/react';
 import styles from './actorPage.module.scss';
 
-import * as Sentry from '@sentry/browser';
-
 export class ActorPage extends Component {
 	render() {
-		Sentry.captureException(new URIError('Our Error'));
 		return (
 			<div className={styles.page}>
 				<main className={styles.main}>
