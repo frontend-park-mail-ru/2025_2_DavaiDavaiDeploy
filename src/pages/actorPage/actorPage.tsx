bd498fcc0bc2a6fc80f867b9e9b607f39d21a1f5
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-10-31 23:45:24 +0300

    chore: sentry test

--- src/pages/actorPage/actorPage.tsx
+++ src/pages/actorPage/actorPage.tsx
@@ -3,8 +3,11 @@ import { FilmSlider } from '@/components/filmSlider/filmSlider';
 import { Component } from '@robocotik/react';
 import styles from './actorPage.module.scss';
 
+import * as Sentry from '@sentry/browser';
+
 export class ActorPage extends Component {
 	render() {
+		Sentry.captureException(new EvalError('Our Error'));
 		return (
 			<div className={styles.page}>
 				<main className={styles.main}>
