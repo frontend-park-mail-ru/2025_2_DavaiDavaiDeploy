9689176066d06cfba5793a8fadf9f47568b82819
Robocotik <alexander.starkin@gmail.com>
2025-11-04 22:38:36 +0300

    [choire] sw: test of /assets/

--- src/sw.js
+++ src/sw.js
@@ -1,21 +1,15 @@
-import { precache } from 'workbox-precaching';
-precache(self.__WB_MANIFEST);
-//eslint-disable-next-line no-console
-console.log('WB: ', self.__WB_MANIFEST);
+// import { precache } from 'workbox-precaching';
+// precache(self.__WB_MANIFEST);
+
+// console.log('WB: ', self.__WB_MANIFEST);
 
 const CACHE_NAME = 'ddd_cache_v1';
-// const CACHE_URLS = [
-// 	'/',
-// 	'/index.html',
-// 	'/assets/index.css',
-// 	'/assets/index.js',
-// 	'/assets/logo.svg',
-// ];
+const CACHE_URLS = ['/', '/index.html', '/assets/'];
 
-// const preCacheResources = async (resources) => {
-// 	const cache = await caches.open(CACHE_NAME);
-// 	await cache.addAll(resources);
-// };
+const preCacheResources = async (resources) => {
+	const cache = await caches.open(CACHE_NAME);
+	await cache.addAll(resources);
+};
 
 const addResponseToCache = async (request, response) => {
 	const cache = await caches.open(CACHE_NAME);
@@ -56,9 +50,9 @@ const networkFirst = async (request) => {
 	}
 };
 
-// self.addEventListener('install', (event) => {
-// 	event.waitUntil(preCacheResources(CACHE_URLS));
-// });
+self.addEventListener('install', (event) => {
+	event.waitUntil(preCacheResources(CACHE_URLS));
+});
 
 self.addEventListener('activate', async () => {
 	const cacheNames = await caches.keys();
