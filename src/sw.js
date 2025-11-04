cd80bf37bf40a0159256bee593e57cac4cde6222
Robocotik <alexander.starkin@gmail.com>
2025-11-04 15:49:26 +0300

    [choire] try to make cache all get

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,5 @@
-const CACHE = 'ddfilms-static-v1';
+const CACHE_STATIC = 'ddfilms-static-v1';
+const CACHE_DYNAMIC = 'ddfilms-dynamic-v1';
 
 const STATIC_ASSETS = [
 	'/',
@@ -13,7 +14,7 @@ const STATIC_ASSETS = [
 
 self.addEventListener('install', (event) => {
 	event.waitUntil(
-		caches.open(CACHE).then((cache) => {
+		caches.open(CACHE_STATIC).then((cache) => {
 			return cache.addAll(STATIC_ASSETS).catch((error) => {
 				throw new Error(error);
 			});
@@ -30,7 +31,7 @@ self.addEventListener('activate', (event) => {
 			caches.keys().then((keys) =>
 				Promise.all(
 					keys.map((key) => {
-						if (key !== CACHE) {
+						if (key !== CACHE_STATIC && key !== CACHE_DYNAMIC) {
 							return caches.delete(key);
 						}
 					}),
@@ -45,12 +46,18 @@ self.addEventListener('fetch', (event) => {
 		return;
 	}
 
+	const requestUrl = new URL(event.request.url);
+
+	// Определяем какой кеш использовать
+	const isLocalResource = requestUrl.origin === self.location.origin;
+	const cacheName = isLocalResource ? CACHE_STATIC : CACHE_DYNAMIC;
+
 	event.respondWith(
 		fetch(event.request)
 			.then((response) => {
 				if (response && response.ok) {
 					const responseToCache = response.clone();
-					caches.open(CACHE).then((cache) => {
+					caches.open(cacheName).then((cache) => {
 						cache.put(event.request, responseToCache);
 					});
 				}
@@ -58,6 +65,7 @@ self.addEventListener('fetch', (event) => {
 				return response;
 			})
 			.catch(() => {
+				// Ищем в обоих кешах
 				return caches.match(event.request).then((cached) => {
 					if (cached) {
 						return cached;
