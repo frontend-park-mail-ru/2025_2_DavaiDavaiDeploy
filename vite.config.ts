a777f8a327c2a01f9f3ea8b444602aae41f9b200
Robocotik <alexander.starkin@gmail.com>
2025-11-05 21:56:21 +0300

    [feat] vite: add copy of favs

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,10 @@ export default ({ mode }: ConfigEnv) => {
 						src: 'src/assets/screenshots/',
 						dest: './assets',
 					},
+					{
+						src: 'src/assets/favicon/',
+						dest: './assets',
+					},
 				],
 			}),
 			sentryVitePlugin({
