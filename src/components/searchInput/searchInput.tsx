4df8a999dd2c2338f038d181468e5c9641b43eeb
Robocotik <alexander.starkin@gmail.com>
2025-11-24 16:27:53 +0300

    [stash]

--- src/components/searchInput/searchInput.tsx
+++ src/components/searchInput/searchInput.tsx
@@ -5,17 +5,28 @@ import { compose, connect } from '@/modules/redux';
 import type { Dispatch } from '@/modules/redux/types/actions.ts';
 import actions from '@/redux/features/search/actions.ts';
 import type { Map } from '@/types/map';
+import { Badge, Headline, Image } from '@/uikit';
 import { Flex, IconButton } from '@/uikit/index';
 import { Component } from '@robocotik/react';
+import { debounce } from '../../helpers/debounceHelper/debounceHelper';
+import { formatRating } from '../../helpers/ratingFormatHelper/ratingFormatHelper';
+import { getRatingType } from '../../helpers/ratingTypeHelper/ratingTypeHelper';
+import type { State } from '../../modules/redux/types/store';
+import { Link } from '../../modules/router/link';
 import type { WithRouterProps } from '../../modules/router/types/withRouterProps.ts';
 import { withRouter } from '../../modules/router/withRouter.tsx';
+import { selectSearchResult } from '../../redux/features/search/selectors';
+import type { ModelsSearchResponse } from '../../types/models';
 import styles from './searchInput.module.scss';
 
+const DEBOUNCE_DELAY = 150;
 interface SearchInputProps {
 	getSearchResult: (searchRequest: string) => void;
+	getHintResult: (searchRequest: string) => void;
 	onClose?: VoidFunction;
 	type: 'small' | 'big';
 	className: string;
+	hintResult: ModelsSearchResponse;
 }
 
 interface SearchInputState {
@@ -29,6 +40,10 @@ class SearchInputComponent extends Component<
 	state = {
 		searchRequest: '',
 	};
+
+	debouncedSearch = debounce((search) => {
+		this.props.getHintResult(search);
+	}, DEBOUNCE_DELAY);
 
 	search = () => {
 		if (this.state.searchRequest === '') {
@@ -44,6 +59,8 @@ class SearchInputComponent extends Component<
 
 	handleSearchRequestChange = (event: InputEvent) => {
 		const value = (event.target as HTMLInputElement).value;
+		this.debouncedSearch(value);
+
 		this.setState({ searchRequest: value });
 	};
 
@@ -56,29 +73,98 @@ class SearchInputComponent extends Component<
 	render() {
 		if (this.props.type === 'big') {
 			return (
-				<Flex
-					className={clsx(styles.searchInput, this.props.className, {
-						[styles.active]: this.state.searchRequest !== '',
-					})}
-					direction="row"
-					align="center"
-				>
-					<input
-						type="text"
-						placeholder="Поиск фильмов, актеров..."
-						value={this.state.searchRequest}
-						onInput={this.handleSearchRequestChange}
-						className={styles.input}
-						onKeyDown={this.handleKeyDown}
-					></input>
-					<IconButton
-						mode="tertiary"
-						className={styles.loupeBtn}
-						onClick={this.props.onClose}
+				<div className={styles.bigSearchWrapper}>
+					<Flex
+						className={clsx(styles.searchInput, this.props.className, {
+							[styles.active]: this.state.searchRequest !== '',
+						})}
+						direction="row"
+						align="center"
 					>
-						<Loupe className={styles.loupe} />
-					</IconButton>
-				</Flex>
+						<input
+							type="text"
+							placeholder="Поиск фильмов, актеров..."
+							value={this.state.searchRequest}
+							onInput={this.handleSearchRequestChange}
+							className={styles.input}
+							onKeyDown={this.handleKeyDown}
+						></input>
+						<IconButton
+							mode="tertiary"
+							className={styles.loupeBtn}
+							onClick={this.props.onClose}
+						>
+							<Loupe className={styles.loupe} />
+						</IconButton>
+					</Flex>
+					{this.state.searchRequest !== '' && (
+						<div className={styles.hint}>
+							<div className={styles.hintWrapper}>
+								{(!this.props.hintResult.films ||
+									!this.props.hintResult.actors) && <p>Нет результатов</p>}
+
+								<div className={styles.hintContent}>
+									<h1>Фильмы</h1>
+									{this.props.hintResult.films &&
+										this.props.hintResult.films.length > 0 && (
+											<ul>
+												{this.props.hintResult.films.map((film) => {
+													const formattedRating = formatRating(film.rating);
+													const ratingType = getRatingType(film.rating);
+													return (
+														<Link href={`/films/${film.id}`}>
+															<li>
+																<Image
+																	className={styles.image}
+																	src={film.cover}
+																	alt={film.title}
+																/>
+																<div>
+																	<div>
+																		<h3>{film.title}</h3>
+																		{ratingType && (
+																			<Badge
+																				mode={ratingType}
+																				className={
+																					styles[`rating-${ratingType}`]
+																				}
+																			>
+																				<Headline level="7">
+																					{formattedRating}
+																				</Headline>
+																			</Badge>
+																		)}
+																	</div>
+																	<div>
+																		<p>{film.genre}</p>
+																		<p>{film.year}</p>
+																	</div>
+																</div>
+															</li>
+														</Link>
+													);
+												})}
+											</ul>
+										)}
+								</div>
+								<div className={styles.hintContent}>
+									<h1>Актеры</h1>
+									{this.props.hintResult.actors &&
+										this.props.hintResult.actors.length > 0 && (
+											<ul>
+												{this.props.hintResult.actors.map((actor) => (
+													<li key={actor.id}>
+														<img src={actor.photo} alt={actor.russian_name} />
+														<p>{actor.russian_name}</p>
+													</li>
+												))}
+											</ul>
+										)}
+								</div>
+							</div>
+						</div>
+					)}
+				</div>
 			);
 		}
 
@@ -121,9 +207,15 @@ class SearchInputComponent extends Component<
 const mapDispatchToProps = (dispatch: Dispatch): Map => ({
 	getSearchResult: (searchRequest: string) =>
 		dispatch(actions.getSearchResultAction(searchRequest)),
+	getHintResult: (searchRequest: string) =>
+		dispatch(actions.getSearchResultAction(searchRequest)),
+});
+
+const mapStateToProps = (state: State): Map => ({
+	hintResult: selectSearchResult(state),
 });
 
 export const SearchInput = compose(
 	withRouter,
-	connect(null, mapDispatchToProps),
+	connect(mapStateToProps, mapDispatchToProps),
 )(SearchInputComponent);
