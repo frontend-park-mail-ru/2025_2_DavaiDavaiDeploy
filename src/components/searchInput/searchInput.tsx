199e326083149325aa624084a7f700effa547d12
Robocotik <alexander.starkin@gmail.com>
2025-11-25 23:46:18 +0300

    [stash]

--- src/components/searchInput/searchInput.tsx
+++ src/components/searchInput/searchInput.tsx
@@ -7,11 +7,7 @@ import actions from '@/redux/features/search/actions.ts';
 import type { Map } from '@/types/map';
 import { Flex, IconButton } from '@/uikit/index';
 import { Component } from '@robocotik/react';
-import { MediaRecorder, register } from 'extendable-media-recorder';
-import { connect as WAVConnect } from 'extendable-media-recorder-wav-encoder';
-import { MICROPHONE_STATES } from '../../consts/microphone';
 import { debounce } from '../../helpers/debounceHelper/debounceHelper';
-import { getMicrophoneIconFromState } from '../../helpers/getMicrophoneIconFromState/getMicrophoneIconFromState';
 import type { State } from '../../modules/redux/types/store';
 import type { WithRouterProps } from '../../modules/router/types/withRouterProps.ts';
 import { withRouter } from '../../modules/router/withRouter.tsx';
@@ -21,15 +17,16 @@ import {
 } from '../../redux/features/search/selectors';
 import type { ModelsSearchResponse } from '../../types/models';
 import { SearchSuggest } from '../SearchSuggest/SearchSuggest';
+import { SearchVoice } from '../SearchVoice/SearchVoice';
 import styles from './searchInput.module.scss';
 
 const DEBOUNCE_DELAY = 150;
 const DEBOUNCE_RESIZE_DELAY = 300;
 const MIN_SEARCH_LENGTH = 3;
+
 interface SearchInputProps {
 	getSearchResult: (searchRequest: string) => void;
 	getHintResult: (searchRequest: string) => void;
-	getVoiceSearchResult: (searchWAV: Blob) => void;
 	clearVoiceSearch: VoidFunction;
 	onClose?: VoidFunction;
 	type: 'small' | 'big';
@@ -42,26 +39,17 @@ interface SearchInputState {
 	searchRequest: string;
 	isSuggestVisible: boolean;
 	prevSearchRequest: string;
-	microphoneState: (typeof MICROPHONE_STATES)[keyof typeof MICROPHONE_STATES];
 }
-
-await register(await WAVConnect());
-
-const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-const mediaRecorder = new MediaRecorder(stream, { mimeType: 'audio/wav' });
 
 class SearchInputComponent extends Component<
 	SearchInputProps & WithRouterProps,
 	SearchInputState
 > {
-	state = {
+	state: SearchInputState = {
 		searchRequest: '',
 		isSuggestVisible: false,
 		prevSearchRequest: '',
-		microphoneState: MICROPHONE_STATES.INACTIVE,
 	};
-
-	audioChunks: Blob[] = [];
 
 	handleResize = () => {
 		this.setState({ isSuggestVisible: false });
@@ -71,35 +59,11 @@ class SearchInputComponent extends Component<
 
 	onMount() {
 		window.addEventListener('resize', this.debouncedResize);
-
-		mediaRecorder.ondataavailable = (event) => {
-			if (event.data.size > 0) {
-				this.audioChunks.push(event.data);
-			}
-		};
-
-		mediaRecorder.onstop = () => {
-			const audioBlob = new Blob(this.audioChunks, {
-				type: 'audio/wav',
-			});
-
-			this.audioChunks = [];
-
-			this.props.getVoiceSearchResult(audioBlob);
-		};
 	}
+
 	onUnmount() {
 		window.removeEventListener('resize', this.debouncedResize);
 	}
-
-	handleStartRecording = () => {
-		this.audioChunks = [];
-		mediaRecorder.start();
-	};
-
-	handleStopRecording = () => {
-		mediaRecorder.stop();
-	};
 
 	onUpdate() {
 		if (
@@ -111,20 +75,6 @@ class SearchInputComponent extends Component<
 			this.props.clearVoiceSearch();
 		}
 	}
-
-	handleMicrophoneClick = () => {
-		if (this.state.microphoneState === MICROPHONE_STATES.INACTIVE) {
-			this.setState({ microphoneState: MICROPHONE_STATES.ACTIVE });
-			this.handleStartRecording();
-			return;
-		}
-
-		if (this.state.microphoneState === MICROPHONE_STATES.ACTIVE) {
-			this.setState({ microphoneState: MICROPHONE_STATES.LOADING });
-			this.handleStopRecording();
-			this.setState({ microphoneState: MICROPHONE_STATES.INACTIVE });
-		}
-	};
 
 	debouncedSearch = debounce((search) => {
 		this.props.getHintResult(search);
@@ -163,15 +113,12 @@ class SearchInputComponent extends Component<
 		const value = (event.target as HTMLInputElement).value;
 
 		if (value.length >= MIN_SEARCH_LENGTH) {
-			this.debouncedSearch(value);
 			this.setState({ isSuggestVisible: true });
-		}
-
-		if (this.state.isSuggestVisible && value.length < MIN_SEARCH_LENGTH) {
+			this.debouncedSearch(value);
+		} else {
 			this.setState({ isSuggestVisible: false });
 		}
 
-		this.setState({ prevSearchRequest: this.state.searchRequest });
 		this.setState({ searchRequest: value });
 	};
 
@@ -181,11 +128,7 @@ class SearchInputComponent extends Component<
 
 	handleKeyDown = (event: KeyboardEvent) => {
 		if (event.key === 'Enter') {
-			if (this.state.prevSearchRequest !== this.state.searchRequest) {
-				this.search();
-			}
-
-			this.setState({ isSuggestVisible: false });
+			this.search();
 		}
 	};
 
@@ -208,13 +151,9 @@ class SearchInputComponent extends Component<
 							className={styles.input}
 							onKeyDown={this.handleKeyDown}
 						></input>
-						<IconButton
-							mode="tertiary"
-							className={styles.loupeBtn}
-							onClick={this.handleMicrophoneClick}
-						>
-							{getMicrophoneIconFromState(this.state.microphoneState)}
-						</IconButton>
+
+						<SearchVoice className={styles.loupeBtn} />
+
 						<IconButton
 							mode="tertiary"
 							className={styles.loupeBtn}
@@ -242,6 +181,7 @@ class SearchInputComponent extends Component<
 					direction="row"
 					align="center"
 				>
+					<SearchVoice className={styles.loupeBtn} />
 					<IconButton
 						mode="tertiary"
 						className={styles.loupeBtn}
@@ -277,19 +217,17 @@ class SearchInputComponent extends Component<
 	}
 }
 
+const mapStateToProps = (state: State): Map => ({
+	hintResult: selectSearchResult(state),
+	voiceSearchResult: selectVoiceSearchResult(state),
+});
+
 const mapDispatchToProps = (dispatch: Dispatch): Map => ({
 	getSearchResult: (searchRequest: string) =>
 		dispatch(actions.getSearchResultAction(searchRequest)),
 	getHintResult: (searchRequest: string) =>
 		dispatch(actions.getSearchResultAction(searchRequest)),
-	getVoiceSearchResult: (searchRequest: Blob) =>
-		dispatch(actions.getVoiceSearchResultAction(searchRequest)),
 	clearVoiceSearch: () => dispatch(actions.clearVoiceSearchResultAction()),
-});
-
-const mapStateToProps = (state: State): Map => ({
-	hintResult: selectSearchResult(state),
-	voiceSearchResult: selectVoiceSearchResult(state),
 });
 
 export const SearchInput = compose(
