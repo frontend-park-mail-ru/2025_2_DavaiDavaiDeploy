41b57cc170026282bb8f41eda10a1a22b257f661
Robocotik <alexander.starkin@gmail.com>
2025-11-25 23:50:00 +0300

    [feat] voiceSearch: add on mobile

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -22,6 +22,7 @@ import { withModal } from '../../modules/modals/withModal.tsx';
 import type { WithModalProps } from '../../modules/modals/withModalProps.ts';
 import { withRouter } from '../../modules/router/withRouter.tsx';
 import { SearchInput } from '../searchInput/searchInput.tsx';
+import { SearchVoice } from '../SearchVoice/SearchVoice';
 import styles from './header.module.scss';
 
 interface HeaderProps {
@@ -105,6 +106,7 @@ class HeaderComponent extends Component<
 				</Link>
 				<Flex className={styles.right} direction="row" align="center">
 					<SearchInput type="big" className={styles.bigSearch} />
+					<SearchVoice className={styles.voiceSearch} />
 					<IconButton
 						mode="tertiary"
 						className={styles.loupeBtn}
