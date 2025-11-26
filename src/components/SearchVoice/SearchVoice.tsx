add5ad61d9be4a6bd136083f5ca60f8b7ede965d
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-11-26 17:15:42 +0300

    Revert "feat: add micro access handling"
    This reverts commit 4385806c177a5ae182e172a9e6865f7085fdaf61.

--- src/components/SearchVoice/SearchVoice.tsx
+++ src/components/SearchVoice/SearchVoice.tsx
@@ -23,7 +23,6 @@ interface SearchVoiceState {
 	mediaRecorder: IMediaRecorder | null;
 	stream: MediaStream | null;
 	audioChunks: Blob[];
-	isMicrophoneAvailable: boolean;
 }
 
 class SearchVoiceComponent extends Component<
@@ -35,36 +34,13 @@ class SearchVoiceComponent extends Component<
 		mediaRecorder: null,
 		stream: null,
 		audioChunks: [],
-		isMicrophoneAvailable: true,
 	};
 
 	async onMount() {
 		try {
 			await register(await WAVConnect());
-			await this.setupMicrophoneAccessListener();
 		} catch {}
 	}
-
-	setupMicrophoneAccessListener = async () => {
-		if (!navigator.permissions || !navigator.permissions.query) {
-			return;
-		}
-
-		try {
-			const permissionStatus = await navigator.permissions.query({
-				name: 'microphone' as PermissionName,
-			});
-
-			const handlePermissionChange = () => {
-				this.setState({
-					isMicrophoneAvailable: permissionStatus.state === 'granted',
-				});
-			};
-
-			permissionStatus.addEventListener('change', handlePermissionChange);
-			handlePermissionChange();
-		} catch {}
-	};
 
 	onUnmount() {
 		this.cleanupRecording();
@@ -146,10 +122,6 @@ class SearchVoiceComponent extends Component<
 	};
 
 	render() {
-		if (!this.state.isMicrophoneAvailable) {
-			return <div />;
-		}
-
 		return (
 			<IconButton
 				mode="tertiary"
