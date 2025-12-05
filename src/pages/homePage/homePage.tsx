1f120e75912d4f7a57111faeab9a02e47f537e58
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-12-05 14:39:49 +0300

    feat: add recommendations

--- src/pages/homePage/homePage.tsx
+++ src/pages/homePage/homePage.tsx
@@ -1,13 +1,36 @@
 import { CalendarWidget } from '@/components/calendarWidget/calendarWidget';
 import { CompilationWidget } from '@/components/compilationWidget/compilationWidget';
 import { FilmCardGrid } from '@/components/filmCardGrid/filmCardGrid';
+import { FilmsLine } from '@/components/filmsLine/filmsLine';
 import { GenreSlider } from '@/components/genreSlider/genreSlider';
 import { PromoFilm } from '@/components/promoFilm/promoFilm';
+import { compose, connect } from '@/modules/redux';
+import type { Dispatch } from '@/modules/redux/types/actions';
+import type { State } from '@/modules/redux/types/store';
+import { withRouter } from '@/modules/router/withRouter';
+import actions from '@/redux/features/films/actions';
+import { selectRecommendations } from '@/redux/features/films/selectors';
+import type { Map } from '@/types/map';
+import type { ModelsMainPageFilm } from '@/types/models';
 import { Flex } from '@/uikit/index';
 import { Component } from '@robocotik/react';
 import styles from './homePage.module.scss';
 
-export class HomePage extends Component {
+interface HomePageProps {
+	recommendations: ModelsMainPageFilm[] | null;
+	clearFilms: VoidFunction;
+	getReccomendations: VoidFunction;
+}
+
+export class HomePageComponent extends Component<HomePageProps> {
+	onMount() {
+		this.props.getReccomendations();
+	}
+
+	onUnmount() {
+		this.props.clearFilms();
+	}
+
 	render() {
 		return (
 			<Flex className={styles.page} direction="column">
@@ -15,6 +38,11 @@ export class HomePage extends Component {
 					<PromoFilm />
 					<GenreSlider />
 					<CompilationWidget />
+					<FilmsLine
+						films={this.props.recommendations}
+						title="Специально для вас"
+						className={styles.recs}
+					/>
 					<CalendarWidget />
 					<Flex className={styles.films} direction="column">
 						<FilmCardGrid />
@@ -24,3 +52,17 @@ export class HomePage extends Component {
 		);
 	}
 }
+
+const mapStateToProps = (state: State): Map => ({
+	recommendations: selectRecommendations(state),
+});
+
+const mapDispatchToProps = (dispatch: Dispatch): Map => ({
+	clearFilm: () => dispatch(actions.clearFilmsAction()),
+	getReccomendations: () => dispatch(actions.getRecommendationsAction()),
+});
+
+export const HomePage = compose(
+	withRouter,
+	connect(mapStateToProps, mapDispatchToProps),
+)(HomePageComponent);
