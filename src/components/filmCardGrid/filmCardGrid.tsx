356fe3951f5eff03f4298caeaedb4d6cab8ed575
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-12-06 22:43:57 +0300

    refactor: remove entities

--- src/components/filmCardGrid/filmCardGrid.tsx
+++ src/components/filmCardGrid/filmCardGrid.tsx
@@ -29,16 +29,9 @@ class FilmCardGridComponent extends Component<
 	onMount() {
 		this.props.getFilms(this.props.cursor);
 
-		this.observer = new IntersectionObserver(
-			(entries) => {
-				const entry = entries[0];
-
-				if (entry.isIntersecting) {
-					this.loadMoreFilms();
-				}
-			},
-			{ rootMargin: ROOT_MARGIN },
-		);
+		this.observer = new IntersectionObserver(this.loadMoreFilms, {
+			rootMargin: ROOT_MARGIN,
+		});
 
 		if (this.loadMoreTriggerRef.current && this.observer) {
 			this.observer.observe(this.loadMoreTriggerRef.current);
