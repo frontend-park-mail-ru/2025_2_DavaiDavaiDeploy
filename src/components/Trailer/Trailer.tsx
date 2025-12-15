e17a9d71e58a10e7cd201451a81bf1d25a9cb6b6
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-12-15 18:29:20 +0300

    chore: remove test

--- src/components/Trailer/Trailer.tsx
+++ src/components/Trailer/Trailer.tsx
@@ -19,7 +19,7 @@ export class TrailerComponent extends Component<
 > {
 	handleTrailerClick = () => {
 		this.props.modal.open(MODALS.TRAILER_MODAL, {
-			videoSrc: getImageURL('trailers/trailer1.mov'),
+			videoSrc: getImageURL(this.props.videoSrc),
 		});
 	};
 
